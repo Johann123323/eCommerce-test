aa335139235bb86d34acb2a4eade5e0fb0aabd55
Johannes <johygogo@gmail.com>
2023-08-23 01:04:26 -0300

    Se agrego la navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarruselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
